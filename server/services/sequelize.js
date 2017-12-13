de4bae3eb5f5cdea171de350407d7cffcf81725e
Yurii Hupalo <yurii.hupalo@code-care.pro>
2017-12-13 20:14:07 +0200

    Extract db credentials to config

--- server/services/sequelize.js
+++ server/services/sequelize.js
@@ -1,8 +1,9 @@
 const Sequelize = require('sequelize');
+const { db } = require('config');
 
-const sequelize = new Sequelize('electronics', 'xineman', 'air228', {
-  host: 'localhost',
-  port: 3306,
+const sequelize = new Sequelize(db.name, db.user, db.pass, {
+  host: db.host,
+  port: db.port,
   dialect: 'mysql',
 });
 
