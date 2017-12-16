a1584583853e28c068d21f3bd18a37169a672916
xineman <uragupalo@gmail.com>
2017-12-16 23:34:06 +0200

    Fix cart and with api urls

--- src/api/products.js
+++ src/api/products.js
@@ -5,14 +5,3 @@ export const getAll = () =>
 
 export const getOne = id =>
   axios.get(`api/products/${id}`);
-
-export const cart = (id, count) =>
-  axios.post('/api/products/cart', {
-    id,
-    count,
-  });
-
-export const wish = id =>
-  axios.post('/api/products/wish', {
-    id,
-  });
--- src/api/user.js
+++ src/api/user.js
@@ -5,3 +5,14 @@ export const signIn = data => axios.post('/api/auth/login', data);
 export const signUp = data => axios.post('/api/auth/register', data);
 
 export const getWishes = () => axios.get('/api/user/wishes');
+
+export const cart = (id, count) =>
+  axios.post('/api/user/cart', {
+    id,
+    count,
+  });
+
+export const wish = id =>
+  axios.post('/api/user/wish', {
+    id,
+  });
